99be41895eabd81ad7e229d46554f6b6dc421602
“Mahesh <maheshpabeykoon@gmail.com?>
2022-08-13 20:43:31 +0530

    Create GET request

--- src/main/java/com/springboot/customermanagment/controller/CustomerController.java
+++ src/main/java/com/springboot/customermanagment/controller/CustomerController.java
@@ -1,8 +1,9 @@
 package com.springboot.customermanagment.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,18 @@ public class CustomerController {
     @Autowired
 	CustomerService customerService;
 	
+    @GetMapping("/showCustomers")
+    public List <Customer> getAllCustomer(){
+        return customerService.getAllDetails();
+    }
+
 	@PostMapping("/customers")
 	public Customer saveCustomerDetails (@RequestBody Customer customer) {
 		
         return customerService.saveCustomer(customer);
 		
 	}
+
+
+
 }
--- src/main/java/com/springboot/customermanagment/service/CustomerService.java
+++ src/main/java/com/springboot/customermanagment/service/CustomerService.java
@@ -1,10 +1,13 @@
 package com.springboot.customermanagment.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.customermanagment.model.Customer;
 import com.springboot.customermanagment.repository.CustomerRepository;
+
 
 @Service
 public class CustomerService {
@@ -13,5 +16,9 @@ public class CustomerService {
 
     public Customer saveCustomer(Customer customer){
         return customerRepository.save(customer);
+    }
+
+    public List <Customer> getAllDetails(){
+    return customerRepository.findAll();       
     }
 }
