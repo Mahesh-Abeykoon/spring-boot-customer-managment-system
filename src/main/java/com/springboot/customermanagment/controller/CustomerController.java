0a1cdab2fa4c1a0c035675969b3d79cf98f50850
“Mahesh <maheshpabeykoon@gmail.com?>
2022-08-13 21:22:12 +0530

    Create PUT request

--- src/main/java/com/springboot/customermanagment/controller/CustomerController.java
+++ src/main/java/com/springboot/customermanagment/controller/CustomerController.java
@@ -1,10 +1,13 @@
 package com.springboot.customermanagment.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +30,17 @@ public class CustomerController {
         return customerService.saveCustomer(customer);
 		
 	}
+
+    @GetMapping("/customer/{id}")
+    public Optional<Customer> getCustomer(@PathVariable Long id){
+        return customerService.getCustomerById(id);
+
+    }
+
+    @PutMapping("/customer/{id}")
+    public Customer updateCustomer( @PathVariable Long id, @RequestBody Customer customer){
+        return customerService.updateCustomerById(id, customer);
+    }
 
 
 
